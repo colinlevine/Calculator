651614b279c410825016c44a50cc964e45f8a1e2
colinlevine <levc1244@gmail.com>
2023-07-23 23:50:06 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+numberOfButtons = document.querySelectorAll("button").length
+var display ="";
+
+for (var i=0; i<numberOfButtons; i++) { //Click event listener
+    document.querySelectorAll("button")[i].addEventListener("click", function () {
+        buttonName = this.innerHTML //Check button name
+        
+        if (buttonName == "="){ //= is pressed
+            answer = calculator(display)
+            console.log(answer)
+            display = answer
+            document.querySelector(".output").innerText = display;
+        }
+        else if (buttonName == "Clear"){ //Clear is pressed; resets display to blank
+            display = "";
+            document.querySelector(".output").innerHTML = "<br>";
+        }
+        else { //When = has not been pressed
+            display += buttonName; //Concatenates buttonName to the number that is displayed
+            document.querySelector(".output").innerText = display;
+        }
+        
+    });
+}
+
+function calculator(display) { //Calculator that converts string to math
+    if (display.includes("+")){
+        display = display.split("+");
+        answer = parseFloat(display[0])+parseFloat(display[1])
+    }
+    else if (display.includes("-")){
+        display = display.split("-");
+        answer = parseFloat(display[0])-parseFloat(display[1])
+    }
+    else if (display.includes("*")){
+        display = display.split("*");
+        answer = parseFloat(display[0])*parseFloat(display[1])
+    }
+    else if (display.includes("/")){
+        display = display.split("/");
+        answer = parseFloat(display[0])/parseFloat(display[1])
+    }
+    return answer
+}
